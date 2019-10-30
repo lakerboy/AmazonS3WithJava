591522f49ffad640bb1f7db2fead21c411d01bec
val <valgrinberg@hotmail.com>
2019-10-29 21:50:39 -0700

    Full project commit

---
+++ AmazonS3WithJava/src/main/java/com/aws/s3/constants/CommonConstants.java
@@ -0,0 +1,24 @@
+package com.aws.s3.constants;
+
+public class CommonConstants {
+	
+	public static String ACCESS_KEY_ID = " AKIAIQKCHU7CVFUVG4QQ";
+	
+	public static String ACCESS_SEC_KEY = "GZVbcZIAmByGAMvXuuxFOACQL1ximOodwNbkPlul";
+	
+	public static String BUCKET_NAME = "vals1234bkt";
+	
+	public static String FOLDER_NAME = "storeFiles";
+	
+	public static String FILE_NAME = "test.tif";
+	
+	public static String FILE_PATH = "C:\\\\Users\\\\Val\\\\Desktop\\\\Attachments\\\\\\\\test.tif";
+	
+	public static String BUCKET_FILE_PATH = "storeFiles/test.tif";
+	
+	public static String LOCAL_DOWNLOAD_PATH = "C:\\\\Users\\\\Val\\\\Desktop\\\\Attachments\\\\\\\\test.tif";
+	
+	public static final String SUFFIX = "/";
+	
+
+}
